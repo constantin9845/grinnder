deb907900be9281cda33ee0367a35049f8388134
constantin9845 <constantin.y@outlook.com>
2026-08-19 22:45:45 +0900

    setup async gather direct using python fallback for testing

--- grinnder/buffer/host.py
+++ grinnder/buffer/host.py
@@ -435,6 +435,8 @@ class HostBuffer:
             else:
                 # Load full target partition --> whole file
                 offset = num_nodes[pid]
+                print(f"{self._backend._storage_dir}{self._file_prefix}_p{pid}.pt")
+                exit(1)
                 self._backend.gpu_read(
                     file_id=f"{self._backend._storage_dir}{self._file_prefix}_p{pid}.pt",
                     tensor=gpu_target[:offset],
