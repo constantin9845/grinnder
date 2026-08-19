f1914c07b9311e1614561be38bb313696ffb42a2
constantin9845 <constantin.y@outlook.com>
2026-08-19 11:07:55 +0900

    test async gather path

--- grinnder/engine/trainer.py
+++ grinnder/engine/trainer.py
@@ -201,8 +201,8 @@ class Trainer:
         for pid in range(self.graph.num_parts):
             start = time.perf_counter()
             feat = self.graph.partition_features(pid)
-            print(feat)
-            print(feat.shape)
+            #print(feat)
+            #print(feat.shape)
             self.host_features[0][pid].copy_(feat)
             del feat
             gc.collect()
