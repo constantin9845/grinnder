e9655aaaaca2d68e0ab671d400cafed1856a36a3
beaver <beaver@mail.com>
2026-08-21 12:38:54 +0900

    add gds async gather from storage in C++ engine

--- csrc/grinnder_ops.cpp
+++ csrc/grinnder_ops.cpp
@@ -12,7 +12,7 @@ PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
   m.def("d2h_synchronize", &d2h_synchronize, "Wait for D2H thread pool");
 
   // Partition gather/scatter
-  m.def("gather_partitions", &gather_partitions,
+  m.def("gather_partitions_gds", &gather_partitions_gds,
         "Gather features from host partitions to GPU tensor");
   m.def("scatter_partitions", &scatter_partitions,
         "Scatter GPU gradient to host partitions with accumulation");
