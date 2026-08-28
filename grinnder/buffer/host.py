45a05bbb048662a81742bca4b2ea7e85f6eb092a
beaver <beaver@mail.com>
2026-08-28 12:16:19 +0900

    setup direct reads as fallback (non-GDS)

--- grinnder/buffer/host.py
+++ grinnder/buffer/host.py
@@ -423,7 +423,7 @@ class HostBuffer:
 
         # File paths
         file_paths = [
-            f"{self._backend._storage_dir}/{self._file_prefix}_p{i}.pt"
+            f"{self._backend._storage_dir}/{self._file_prefix}_p{i}"
             for i in range(self.num_parts)
         ]
 
