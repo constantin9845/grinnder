b5e452690e62eed74fbffb95342bfbd7434df28c
beaver <beaver@mail.com>
2026-09-07 12:09:08 +0900

    compared loading time

--- grinnder/engine/trainer.py
+++ grinnder/engine/trainer.py
@@ -525,6 +525,8 @@ class Trainer:
             )
             tn = time.perf_counter_ns()
             stat.load_GPU_timestamp("forward", "gather", t0, tn)
+            print(f"Load time  = {tn}")
+            exit(1)
 
             self.streams.compute.wait_stream(self.streams.h2d[pool_idx])
             if self.config.mode == "grinnder":
